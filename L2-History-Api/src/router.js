46b74065905c668717a303ffcfdc2b445d51ed19
franHellbusch <fghellbusch@gmail.com>
2026-09-15 12:22:43 -0300

    Lecture 2

---
+++ L2-History-Api/src/router.js
@@ -0,0 +1,16 @@
+import { renderAbout } from "./views/about.js";
+import { renderChat } from "./views/chat.js";
+import { renderHome } from "./views/home.js";
+import { renderNotFound } from "./views/notFound.js";
+
+const routes = {
+  "/": renderHome,
+  "/chat": renderChat,
+  "/about": renderAbout,
+};
+
+export function router() {
+  const path = window.location.pathname;
+  const render = routes[path] || renderNotFound;
+  render();
+}
